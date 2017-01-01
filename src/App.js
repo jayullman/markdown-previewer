e1e4c23615a92c2b010bf3c260b065aa015ad395
Jay Ullman <jayullman@gmail.com>
2016-12-31 20:31:09 -0800

    Add two modules: UserInput Output

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import UserInput from './UserInput';
+
 import marked from 'marked';
 import logo from './logo.svg';
 import './App.css';
+
+// Check out FCC React Challenge 32 for basic architecture of this app
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Markdown Previewer</h2>
+          <h3>A freeCodeCamp Project</h3>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Enter text with the markdown langauge and it will be parsed in real time
         </p>
+
+        <UserInput />
+        {/*
+        <Output />
+        */}
       </div>
     );
   }
---
+++ src/UserInput.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class UserInput extends Component {
+
+  render() {
+    return (
+      <div>
+        Hello from UserInput!
+      </div>
+    )
+
+  }
+
+}
+
+
+
+export default UserInput;
